ad8e6c60944ad9fad44120655860175ebeb98742
mochamadramdhan <mochamadramdhan241200@gmail.com>
2022-11-17 20:58:30 +0700

    Edite 1

--- src/component/Headbar.jsx
+++ src/component/Headbar.jsx
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import Logo from '../Screen/Logo';
+import Logo  from '../Screen/Logo';
 import { styled } from '@mui/material/styles';
 
 
